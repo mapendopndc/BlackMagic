08694ee075164f8a8973a77152a16c95263c42c6
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-05-29 23:16:36 -0400

    Initial commit

---
+++ BlackMagic/BlackMagicComponent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace BlackMagic
+{
+    public class BlackMagicComponent : GH_Component
+    {
+
+        private BoidSimulation myBoidSimulation;
+
+        public BlackMagicComponent()
+          : base(
+                "Boids",
+                "Boids",
+                "Living creatures in constant interaction with each other",
+                "Black Magic", "Creatures")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Reset", "Reset", "Reset Simulation", GH_ParamAccess.item);
+            pManager.AddPointParameter("Starting Positions", "StartPos", "Initial boid locations", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Separation FOV", "sFOV", "Collision avoidance field of view", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Separation Strength", "sMag", "Collision avoidance magnitude", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Alignment FOV", "aFOV", "Velocity matching field of view", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Alignment Strength", "aMag", "Velocity matching magnitude", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Cohesion FOV", "cFOV", "Flock centering field of view", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Cohesion Strength", "cMag", "Flock centering magnitude", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Positions", "Positions", "Updated boid positions", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            bool iReset = false;
+            List<Point3d> iStartPos = new List<Point3d>();
+            double iSepFOV = 0;
+            double iSepMag = 0;
+            double iAlFOV = 0;
+            double iAlMag = 0;
+            double iCoFOV = 0;
+            double iCoMag = 0;
+
+            DA.GetData("Reset", ref iReset);
+            DA.GetDataList("Starting Positions", iStartPos);
+            DA.GetData("Separation FOV", ref iSepFOV);
+            DA.GetData("Separation Strength", ref iSepMag);
+            DA.GetData("Alignment FOV", ref iAlFOV);
+            DA.GetData("Alignment Strength", ref iAlMag);
+            DA.GetData("Cohesion FOV", ref iCoFOV);
+            DA.GetData("Cohesion Strength", ref iCoMag);
+
+            if (iReset || myBoidSimulation == null)
+                myBoidSimulation = new BoidSimulation(iStartPos);
+
+            myBoidSimulation.separationFOV = iSepFOV;
+            myBoidSimulation.separationMag = iSepMag;
+            myBoidSimulation.alignmentFOV = iAlFOV;
+            myBoidSimulation.alignmentMag = iAlMag;
+            myBoidSimulation.cohesionFOV = iCoFOV;
+            myBoidSimulation.cohesionMag = iCoMag;
+
+            myBoidSimulation.Update();
+
+            DA.SetDataList("Positions", myBoidSimulation.BoidPts);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("7eb55393-94c9-4bfe-be03-d1e0a28f26a5"); }
+        }
+    }
+}
---
+++ BlackMagic/BoidSimulation.cs
@@ -0,0 +1,204 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackMagic
+{
+    /* 
+     * Boid simulation implementation based on Craig Reynolds' "Flocks, Herds, and Schools; A Distributed Behavioral Model"
+     * 
+     * Simulates the aggregate motion of a flock of birds through three primary behavioral mechanisms:
+     *   1. Collision Avoidance - Separating from nearby flock mates as to avoid any imminent collisions
+     *   2. Velocity Matching - Attempting to match speed and direction of nearby flock mates
+     *   3. Flock Centering - Tendency to move towards geometric centroid of flock
+     */
+
+    class BoidSimulation
+    {
+        public double separationFOV;
+        public double separationMag;
+        public double cohesionFOV;
+        public double cohesionMag;
+        public double alignmentFOV;
+        public double alignmentMag;
+
+        private List<Point3d> boidPts;
+        private List<Vector3d> boidVls;
+        private List<Vector3d> weightedAccelerations;
+
+
+        public BoidSimulation(List<Point3d> startPos)
+        {
+            boidPts = startPos;
+            boidVls = new List<Vector3d>();
+            for (int i = 0; i < boidPts.Count; i++)
+                boidVls.Add(Vector3d.Zero);
+        }
+
+        public List<Point3d> BoidPts
+        {
+            get { return boidPts; }
+        }
+
+        public void Update()
+        {
+            // Initializing accelerations
+            weightedAccelerations = new List<Vector3d>();
+
+            for (int i = 0; i < boidPts.Count; i++)
+                weightedAccelerations.Add(Vector3d.Zero);
+
+            // Computing weighted acceleration vector based on flock behavior mechanisms outlined by Craig Reynolds
+            Separate();
+            Align();
+            Cohere();
+
+            // Move boids according to their weighted accelerations
+            for (int i = 0; i < boidPts.Count; i++)
+            {
+                boidPts[i] += weightedAccelerations[i];
+                boidVls[i] = weightedAccelerations[i];
+            }
+                
+            
+        }
+
+
+        /* Performs collision avoidance on all boid particles in simulation
+         * 
+         * Naive algorithm for collision avoidance achieves goal in O(N^2) time complexity
+         * 
+         * This implementation uses R-tree class from Rhino's library in order to retrieve a list
+         * of boids within a specified radius in time complexity nearing O(M * logM n), where nodes
+         * have maximum of M entries
+         * 
+         * Calculates distances from all boids to their closest counterparts
+         * Adds vector proportional to their proximity to the weighted acceleration
+         * 
+         */
+
+        public void Separate()
+        {
+            // R-Tree initialization
+            RTree rTree = new RTree();
+
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Insert(boidPts[i], i);
+
+            // Array of integer lists to store indexes of the boids within collision radius for all boids in the simulation
+            List<int>[] collisionIndices = new List<int>[boidPts.Count];
+
+            for (int i = 0; i < boidPts.Count; i++)
+                collisionIndices[i] = new List<int>();
+
+            // Spherical range search of R-Tree based on separation field of view
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Search(
+                    new Sphere(boidPts[i], separationFOV),
+                    (sender, args) => { if (i < args.Id) collisionIndices[i].Add(args.Id); });
+
+            for (int i = 0; i < collisionIndices.Length; i++)
+            {
+                foreach (int j in collisionIndices[i])
+                {
+                    Vector3d acceleration = boidPts[j] - boidPts[i];
+                    double distance = acceleration.Length;
+                    if (distance > separationFOV) continue;
+
+                    // Scale acceleration to make it inversely proportional to distance
+                    acceleration *= 0.5 * (distance - separationFOV) / distance;
+                    
+                    weightedAccelerations[i] += acceleration * separationMag;
+                    weightedAccelerations[j] -= acceleration * separationMag;
+                }
+            }
+
+        }
+
+         /* Performs velocity matching on all boid particles in simulation
+         * 
+         * Implemented using R-Trees for fast range search
+         * 
+         * Averages the velocities of neighboring boids and adds it to the weighted acceleration 
+         * 
+         */
+
+        public void Align()
+        {
+            // R-Tree initialization
+            RTree rTree = new RTree();
+
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Insert(boidPts[i], i);
+
+            // Array of integer lists to store indexes of the boids within collision radius for all boids in the simulation
+            List<int>[] collisionIndices = new List<int>[boidPts.Count];
+
+            for (int i = 0; i < boidPts.Count; i++)
+                collisionIndices[i] = new List<int>();
+
+            // Spherical range search of R-Tree based on alignment field of view
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Search(
+                    new Sphere(boidPts[i], alignmentFOV),
+                    (sender, args) => { if (i < args.Id) collisionIndices[i].Add(args.Id); });
+
+            for (int i = 0; i < collisionIndices.Length; i++)
+            {
+                Vector3d acceleration = Vector3d.Zero;
+                foreach (int j in collisionIndices[i])
+                    acceleration += boidVls[j];
+                acceleration -= Vector3d.Zero;
+                acceleration /= (collisionIndices[i].Count == 0) ? 1 : collisionIndices[i].Count;
+                weightedAccelerations[i] += acceleration * alignmentMag;
+            }
+        }
+
+        /* Performs velocity matching on all boid particles in simulation
+        * 
+        * Implemented using R-Trees for fast range search
+        * 
+        * Averages the velocities of neighboring boids and adds it to the weighted acceleration 
+        * 
+        */
+
+        public void Cohere()
+        {
+            // R-Tree initialization
+            RTree rTree = new RTree();
+
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Insert(boidPts[i], i);
+
+            // Array of integer lists to store indexes of the boids within collision radius for all boids in the simulation
+            List<int>[] collisionIndices = new List<int>[boidPts.Count];
+
+            for (int i = 0; i < boidPts.Count; i++)
+                collisionIndices[i] = new List<int>();
+
+            // Spherical range search of R-Tree based on alignment field of view
+            for (int i = 0; i < boidPts.Count; i++)
+                rTree.Search(
+                    new Sphere(boidPts[i], cohesionFOV),
+                    (sender, args) => { if (i < args.Id) collisionIndices[i].Add(args.Id); });
+
+            for (int i = 0; i < collisionIndices.Length; i++)
+            {
+                
+                Point3d centroid = Point3d.Origin;
+                foreach (int j in collisionIndices[i])
+                    centroid += boidPts[j];
+
+                centroid /= (collisionIndices[i].Count == 0) ? 1 : collisionIndices[i].Count;
+                Vector3d acceleration = Vector3d.Zero;
+                acceleration = boidPts[i] - centroid;
+                weightedAccelerations[i] -= acceleration * cohesionMag;
+            }
+        }
+    }
+}
